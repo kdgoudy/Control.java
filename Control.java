4c2b8f146d51323ef529e77b8accab849175e47a
Koki <kgoudy64@gmail.com>
2022-07-04 17:03:12 -0500

    Add files via upload

---
+++ Control.java
@@ -0,0 +1,13 @@
+package Control;
+
+public class Control {
+    public static void main(String[] args) {
+        int time = 20;
+if (time < 18) {
+  System.out.println("Good day.");
+} else {
+  System.out.println("Good evening.");
+}
+// Outputs "Good evening."
+    }
+}
